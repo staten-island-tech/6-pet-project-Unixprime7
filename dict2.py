43bed1bc0ad784919e3c230e324a97af7e481fc1
Unixprime7 <michaeld801@nycstudents.net>
2026-04-01 09:26:29 -0400

    hi

--- dict2.py
+++ dict2.py
@@ -8,6 +8,9 @@ wards = {
 def find_job(guy):
     staff = {}
     for key, value in wards.items():
-        
+        for people in value:
+            staff[people] = [key]
+            if guy == people:
+                return staff[people]
 user_person = input("What person do you want to know about?")
 print(find_job(user_person))
--- pet_project.py
+++ pet_project.py
@@ -11,14 +11,23 @@ class pet:
     def play(self, joy):
         joy += 10
         activities = ['fetch', "tug o' war, chase"]
-        print("You're pet played", activities[random.randint(0,2)], "with you, and gained 10 joy!")
+        print("You're pet played", activities[random.randint(0,2)], "with you, and gained 10 joy points!")
     def wash(self, smelliness):
         smelliness += 10
-        print("You're pet finally isn't filthy! It lost 10 smelliness!")
+        print("You're pet finally isn't filthy! It lost 10 smelliness points!")
     def therapy(self, motivation_to_live):
         motivation_to_live += 10
         print("You're pet underwent therapy and is no longer emo!  It gained 10 motivation_to_live points!")
     def feed(self, hungriness):
         hungriness += 10
         foods = ["dead_rat", "moldy_cheese", "monkey_toenails"]
-        print("You're pet ate some", foods[random.randit(0,2)], "and ")
+        print("You're pet ate some", foods[random.randit(0,2)], "and lost 10 hungriness points!")
+    def grow_up(self, age):
+        age += 1
+        print("You're pet is getting old...It grew up 1 year!")
+    def death(self, age, joy, smelliness, motivation_to_live, hungriness):
+        if age == 20:
+            print("Guess what, your pet was too old and decided life was for noobs!")
+            return age, joy, smelliness, motivation_to_live, hungriness
+        elif joy == 0:
+            print("Your pet ")
